76d7be8ac5ffbdd0f185c4f96a750f71f2f3f7c3
PeekLee <1518757362@qq.com>
2022-04-02 17:09:49 +0800

    LL1驱动程序

--- src/LL1.py
+++ src/LL1.py
@@ -34,13 +34,13 @@ def CreateAnaTable(predict, grammar, only_right):
             for j in only_right:  # 初始化列
                 x[j] = -1
             for g in predict[i + 1]:  # 处理左端无相同的文法
-                x[g] = i + 1
+                x[g] = i
             table_col.append(x)
             pre = grammar[i]['left']
         else:
             row = table_row[grammar[i]['left']]  # 处理左端相同的文法
             for g in predict[i + 1]:
-                table_col[row][g] = i + 1
+                table_col[row][g] = i
     return table_row, table_col
 
 # 初始化
@@ -67,3 +67,40 @@ SignStack.push('Program')
 
 # 建立分析表
 table_row, table_col = CreateAnaTable(predict, grammar, only_right)
+
+# LL1驱动程序
+while not SignStack.isEmpty():
+    sign = SignStack.peek()
+    toke = TokenStack.peek()
+    if toke[1] == 'ID':
+        token = 'ID'
+    elif toke[1] == 'INTC':
+        token = 'INTC'
+    elif toke[1] == 'CHARC':
+        token = 'CHARC'
+    else:
+        token = toke[2]
+    if sign in left: #如果是非终极符，则用语法进行替换
+        row = table_row[sign]
+        judge = table_col[row][token]
+        if judge != -1 :
+            SignStack.pop()
+            rig = grammar[judge]['right']
+            length = len(rig)
+            for i in range(length):
+                if rig[length - 1 - i] != 'NULL':
+                    SignStack.push(rig[length - 1 - i])
+        else:
+            print('error1')
+            break
+    else:
+        if sign == token: #相等则进行匹配
+            SignStack.pop()
+            TokenStack.pop()
+        else:             #不相等出错
+            print('error2')
+            break
+if TokenStack.peek()[2] != 'EOF':
+    print('error3')
+else:
+    print('right')
